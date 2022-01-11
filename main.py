13c2d3f0cae1931da9f9c041988ba4a4ff69b05f
bluisfernandes <>
2022-01-11 05:11:36 +0000

    fix HTTP code request

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# from cs50 import SQL
 from flask import Flask, request, jsonify, render_template, make_response
 import json
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
-
+# Configure application
 app = Flask(__name__)
+
+# Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///finance.db")
 
 
 @app.route("/")
@@ -36,27 +40,62 @@ def location():
 	return render_template("location.html")
 
 
+# Used to communicate with Android APP or colab
 @app.route('/postjson', methods=['POST', 'GET'])
 def postjson():
 	if request.method == "POST":
 		a = request.get_json()
 		
 		if isinstance(a, dict):
-			res = make_response(jsonify(a), 200)
-			print("__response OK")
-			
+			code, message = store_route(a)
+			res = make_response(jsonify(message), code)
+			print(f"__response {code}")
+
 		else:
 			formato = f"must be JSON, its a {type(a)}"
 			res = make_response(jsonify({"message": formato}), 405)
 			print(f"__Response error: {formato}")
 
-
-		print((res.get_json()))
+		# print(type(res.get_json()))
 		
 		return res 
 
 	return '''Try to post a JSON<br>
 					Example in <a href=https://colab.research.google.com/drive/1H-cBSzQcHqKl-CObhL1_tl76_76lynNX?usp=sharing>Colab<a>.'''
+
+
+def store_route(json_request):
+
+	# check if there are all keys
+	try:
+		print("_______________________________")
+		print(json_request["id"])
+		print("______________")
+		print(json_request["device"])
+		print("______________")
+		print(json_request["points"][0])
+		print("______________")
+		print(json_request["info"][0])
+	except:
+		
+		# return code of error and expected json 
+		return 403, {"message":"some parameters are missing","expected":{"id": 10, "device": "xxx", "points":[{"lat": 123.456, "long": 987.654 },{"lat": 123.457, "long": 987.655 }],"info":[{"point_id":0,"route_id":0,"sensor_light":13333.3,"timestamp":"2022-01-11T00:59:53.372"},{"point_id":1,"route_id":0,"sensor_light":13345.9,"timestamp":"2022-01-11T00:59:54.926"}]}}
+	
+	store_id = 0
+	json_request["device"]
+	json_request["id"]
+	json_request["info"][0]["timestamp"]
+	
+	json_request["points"]
+	json_request["info"]
+
+# TODO: store values on sqlite
+# user = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])
+# db.execute("INSERT INTO wallet(user_id, symbol, shares, user_id_symbol) VALUES (?, ?, ?, ?)",
+                      #  session['user_id'], stock['symbol'], shares, user_id_symbol)
+# db.execute("UPDATE wallet SET shares = ? WHERE user_id_symbol = ? ", shares, user_id_symbol)
+	
+	return 200, json_request
 
 
 def errorhandler(e):
